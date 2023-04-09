4e5ed4f8497acf9faab3d80d0a7f22cbff4140a4
Henrik Lampe <h.lampe@hotmail.de>
2023-04-09 15:41:06 +0200

    make button number even and buttons bigger

--- memory.py
+++ memory.py
@@ -2,7 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import Grid
 
-SIZE = 3
+SIZE = 4
+BUTTON_SIZE=50
 
 window = tk.Tk()
 window.title("Memory")
@@ -15,7 +16,7 @@ for x in range(SIZE):
     for y in range(SIZE):
         print(str(x)+" | "+str(y))
         buttons[x][y] = ttk.Button(window, text="Button")
-        buttons[x][y].grid(row=x, column=y, sticky = "NSEW")
+        buttons[x][y].grid(row=x, column=y, ipadx=BUTTON_SIZE, ipady=BUTTON_SIZE, sticky = "NSEW")
         window.columnconfigure(weight=1, index=y)
 
 
