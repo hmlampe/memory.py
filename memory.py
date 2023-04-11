946d67e74b8ab65453ccb5e544339c376e9afb37
Henrik Lampe <h.lampe@hotmail.de>
2023-04-11 12:46:09 +0200

    create own Button class to add functionality

--- memory.py
+++ memory.py
@@ -9,6 +9,32 @@ SIZE = 4
 BUTTON_SIZE=50
 PIC_COUNT = int(SIZE*SIZE/2)
 
+class MemButton(ttk.Button):
+
+    open = False
+
+    def __init__(self, *args, **kwargs):
+        ttk.Button.__init__(self, *args, **kwargs)
+        self['text'] = "Karte"
+        self['command'] = self.change
+
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_picture(self, picture):
+        self.picture = picture
+
+    def change(self):
+        if self.open:
+            self['image'] = None
+            self['text'] = ""
+        else:
+            self['image'] = self.picture
+            self['text'] = "Karte"
+        self.open = not self.open
+        self.update()
+    
 window = tk.Tk()
 window.title("Memory")
 
@@ -30,10 +56,14 @@ for x in range(SIZE):
         i = random.randint(0, pic_count-1)
         path = os.path.join("./pictures", pictures[i])
         photo_images[x][y] = ImageTk.PhotoImage(file=path)
-        buttons[x][y] = ttk.Button(window, image=photo_images[x][y])
+        buttons[x][y] = MemButton(window)
+        buttons[x][y].set_position(x, y)
+        buttons[x][y].set_picture(photo_images[x][y])
         pictures.pop(i)
         pic_count -= 1
-        buttons[x][y].grid(row=x, column=y, sticky = "NSEW")
+        height = 300-buttons[x][y].winfo_reqheight()
+        width = 300-buttons[x][y].winfo_reqwidth()
+        buttons[x][y].grid(row=x, column=y, ipadx=width, ipady=height, sticky = "NSEW")
         window.columnconfigure(weight=1, index=y)
 
 buttons[0][0]
